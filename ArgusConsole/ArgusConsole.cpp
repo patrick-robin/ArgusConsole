5dcb33b3bb5252be9fcc01042b6ea81248ecae7a
Patrick Robin <patrick.robin@gmail.com>
2021-12-20 09:00:32 +0000

    Refactored with suggested changes (https://github.com/argotronic/argus_data_api)

--- ArgusConsole/ArgusConsole.cpp
+++ ArgusConsole/ArgusConsole.cpp
@@ -1,38 +1,74 @@
 // ArgusConsole.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+#include <chrono>
+#include <future>
 #include <iostream>
+#include <string>
+#include <thread>
 #include "argus_monitor_data_accessor.h"
 #include "argus_monitor_data_api.h"
 
+using namespace std::chrono_literals;
+
+void PrintSensorData(argus_monitor::data_api::ArgusMonitorData const& sensor_data, std::size_t const sensor_index)
+{
+    auto const sensor_data_offset = sensor_data.OffsetForSensorType[sensor_index];
+    auto const sensor_data_count = sensor_data.SensorCount[sensor_index];
+
+    for (std::size_t index{}; index < sensor_data_count; ++index) {
+        std::wcout << sensor_data.SensorData[sensor_data_offset + index].Label << L": " << sensor_data.SensorData[sensor_data_offset + index].Value
+            << L' ' << sensor_data.SensorData[sensor_data_offset + index].UnitString << L'\n';
+    }
+}
+
 int main()
 {
-    argus_monitor::data_api::ArgusMonitorDataAccessor data_accessor_{};
+    std::future<void> wait_for_key = std::async([]() { std::cin.get(); });
 
-    bool       callback_called{ false };
+    auto const new_sensor_data_available = [](argus_monitor::data_api::ArgusMonitorData const& new_sensor_data) {
+        std::cout << "New Argus Monitor sensor data available:\n\n";
+        std::cout << "Argus Monitor Version: " << std::to_string(new_sensor_data.ArgusMajor) << "." << std::to_string(new_sensor_data.ArgusMinorA)
+            << "." << std::to_string(new_sensor_data.ArgusMinorB) << "." << std::to_string(new_sensor_data.ArgusBuild) << '\n';
+        std::cout << "Total number of sensor values:" << std::to_string(new_sensor_data.TotalSensorCount) << '\n';
 
-    auto const on_temperature_changed
-        = [&callback_called](argus_monitor::data_api::ArgusMontorData const& new_sensor_data) {
+        // Example: print all CPU temperature values
+        PrintSensorData(new_sensor_data, argus_monitor::data_api::SENSOR_TYPE_CPU_TEMPERATURE);
 
-        // DO ALL THE PROCESSING HERE, ALL DATA IS IN new_sensor_data
-        callback_called = true;
+        // and some extra values (e.g. CCDx temps for AMD processors)
+        PrintSensorData(new_sensor_data, argus_monitor::data_api::SENSOR_TYPE_CPU_TEMPERATURE_ADDITIONAL);
 
-        std::cout << "something\n";
+        // Example2: print all SSD/HDD temperatures
+        PrintSensorData(new_sensor_data, argus_monitor::data_api::SENSOR_TYPE_DISK_TEMPERATURE);
+
+        // Example3: print all GPU temperatures
+        PrintSensorData(new_sensor_data, argus_monitor::data_api::SENSOR_TYPE_GPU_TEMPERATURE);
+
+
+        std::cout << "\n\nWaiting for new sensor data -- press RETURN or Ctrl-C to exit.\n";
     };
 
-    data_accessor_.RegisterSensorCallbackOnDataChanged(on_temperature_changed);
-    data_accessor_.Open();
-    bool isOpen = data_accessor_.IsOpen();
 
-    std::cout << isOpen;
-    std::cout << "\n";
+    argus_monitor::data_api::ArgusMonitorDataAccessor data_accessor_{};
+    data_accessor_.RegisterSensorCallbackOnDataChanged(new_sensor_data_available);
+    while (!data_accessor_.Open()) {
+        std::cout << "Waiting for Argus Monitor API connection to become available\n";
+        if (wait_for_key.wait_for(1s) == std::future_status::ready) {
+            data_accessor_.Close();
+            return -1;
+        }
+    }
 
-    while (callback_called == false)
-    {
-        std::cout << "Waiting\n";
+    std::cout << "Connection to Argus Monitor established\n";
+    std::cout << "Waiting for new sensor data -- press RETURN or Ctrl-C to exit.\n";
+
+    while (wait_for_key.wait_for(1s) != std::future_status::ready) {
+        ;
     }
 
     data_accessor_.Close();
+
+    return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
